0216ca1675c964b6cd8bc84fb9cbe4a9215bae16
Jinai Franklin <jinai.franklin@yahoo.com>
2019-07-25 04:38:35 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,13 +1,16 @@
 def hello_t(array)
   if block_given?
-i = 0
+    i = 0
 
 while i < array.length
-yield array[i]
-i = i + 1
+    yield(array[i])
+    i = i + 1
   end
   
   array
+else 
+  puts "Hey! Np block was given!"
+  end
 end
 
 # call your method here!
